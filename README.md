f8f6ab8bcafb6d1eeb11d54831346209174a1225
Bruno Giarrizzo <bruno@giarrizzo.fr>
2019-02-21 17:07:04 +0100

    Legacy

---
+++ README.md
@@ -0,0 +1,18 @@
+# mailchecker
+
+...
+
+## Support
+
+Support me with a monthly donation and help me continue my activities:
+
+[![liberapay](https://liberapay.com/assets/widgets/donate.svg)](https://liberapay.com/bgiarrizzo/donate)
+
+You can also support me with BTC or ETH :)
+
+* [bitcoin:3HhKsTYg71HPasRXPNPdEnx7nxHzYpgk5W](3HhKsTYg71HPasRXPNPdEnx7nxHzYpgk5W)
+* [etherum:0xD008126040408031E42f0c3dcDa87936C19C12eb](0xD008126040408031E42f0c3dcDa87936C19C12eb)
+
+## License
+
+This project is licensed under the GNU GPL - see the [LICENSE.txt](LICENSE.txt) file for details
---
+++ mailchecker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from validate_email import validate_email
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class MailCheck(BaseHTTPRequestHandler):
+    def do_GET(self):
+        mail = self.path.replace("/", "")
+        mail_is_valid = validate_email(mail, verify=True)
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        if mail_is_valid == None:
+            response = '{"mail_active": false}'
+        else:
+            response = '{"mail_active": %s}' % mail_is_valid
+
+        self.wfile.write(bytes(response, "utf8"))
+        return
+
+
+print("starting server...")
+server_address = ("0.0.0.0", 8081)
+httpd = HTTPServer(server_address, MailCheck)
+print("running server...")
+httpd.serve_forever()
